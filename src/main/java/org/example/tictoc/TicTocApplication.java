b5cd1f05b097f0ea10bcb3110333c0a612bdf3c0
@_.mung <wjdahrrla@gmail.com>
2025-01-11 01:00:58 +0900

    Chore: 스프링부트 프로젝트 세팅해라

---
+++ src/main/java/org/example/tictoc/TicTocApplication.java
@@ -0,0 +1,13 @@
+package org.example.tictoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tictoc/TicTocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tictoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
