f789433946e811770fa1f342f83aa7a5e76fa80c
@_.mung <wjdahrrla@gmail.com>
2025-01-11 23:28:16 +0900

    Chore: UserSchedule 엔티티 세팅해라 - #1

--- src/main/java/org/tictoc/tictoc/domain/user/entity/UserSchedule.java
+++ src/main/java/org/tictoc/tictoc/domain/user/entity/UserSchedule.java
@@ -1,4 +1,30 @@
 package org.tictoc.tictoc.domain.user.entity;
 
-public class UserSchedule {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.tictoc.tictoc.domain.user.entity.type.UserScheduleStatus;
+import org.tictoc.tictoc.global.common.entity.BaseTimeEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSchedule extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long tradeId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private UserScheduleStatus status;
 }
