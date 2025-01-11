ab637e8aca83aab693bf209f6d401c0e23a25ebb
@_.mung <wjdahrrla@gmail.com>
2025-01-11 23:29:45 +0900

    Chore: Profile 엔티티 세팅해라 - #1

--- src/main/java/org/tictoc/tictoc/domain/profile/entity/Profile.java
+++ src/main/java/org/tictoc/tictoc/domain/profile/entity/Profile.java
@@ -1,4 +1,25 @@
 package org.tictoc.tictoc.domain.profile.entity;
 
-public class Profile {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.tictoc.tictoc.global.common.entity.BaseTimeEntity;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private String nickname;
+    private Integer money;
 }
