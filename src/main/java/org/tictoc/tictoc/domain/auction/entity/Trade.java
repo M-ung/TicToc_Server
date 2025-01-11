9a231f14084b966687a6eec40a235834a8d4ecc1
@_.mung <wjdahrrla@gmail.com>
2025-01-11 23:42:05 +0900

    Chore: Trade 엔티티 세팅해라 - #1

---
+++ src/main/java/org/tictoc/tictoc/domain/auction/entity/Trade.java
@@ -0,0 +1,24 @@
+package org.tictoc.tictoc.domain.auction.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.tictoc.tictoc.global.common.entity.BaseTimeEntity;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Trade extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long auctionId;
+    private Long bidId;
+}
