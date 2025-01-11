b87f47516d103042f27a9380c8ef080e3e930aa8
@_.mung <wjdahrrla@gmail.com>
2025-01-11 23:37:17 +0900

    Chore: Auction 엔티티 세팅해라 - #1

---
+++ src/main/java/org/tictoc/tictoc/domain/auction/entity/Auction.java
@@ -0,0 +1,34 @@
+package org.tictoc.tictoc.domain.auction.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.tictoc.tictoc.global.common.entity.BaseTimeEntity;
+import org.tictoc.tictoc.global.common.entity.type.TicTocStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Auction extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long sellerId;
+    private String title;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+    private Integer startPrice;
+    private Integer currentPrice;
+    private Integer finalPrice;
+    private LocalDateTime sellStartTime;
+    private LocalDateTime sellEndTime;
+    private LocalDateTime auctionOpenTime;
+    private LocalDateTime auctionCloseTime;
+    private TicTocStatus status;
+}
