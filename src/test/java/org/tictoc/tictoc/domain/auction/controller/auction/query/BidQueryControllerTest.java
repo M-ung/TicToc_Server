ab06f1e77f83ad2d0f2a959c1ede116f3f8bcb37
@_.mung <wjdahrrla@gmail.com>
2025-01-19 17:50:38 +0900

    Feat: 내 입찰 내역 필터링 조회 API 기능 구현해라 - #32

--- src/test/java/org/tictoc/tictoc/domain/auction/controller/auction/query/BidQueryControllerTest.java
+++ src/test/java/org/tictoc/tictoc/domain/auction/controller/auction/query/BidQueryControllerTest.java
@@ -28,7 +28,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class AuctionQueryControllerTest {
+class BidQueryControllerTest {
     private MockMvc mockMvc;
 
     @InjectMocks
