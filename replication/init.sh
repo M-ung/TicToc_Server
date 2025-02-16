94011759e8421ec012c6279591d5f5661fee447e
M-ung <wjdahrrla@gmail.com>
2025-02-16 23:34:45 +0900

    Docs: Master/Slave 구축에 대한 명령어 정리해라 - #62

--- replication/init.sh
+++ replication/init.sh
@@ -9,10 +9,16 @@
 # MASTER DB
 - docker inspect <컨테이너_이름_또는_ID> | grep "IPAddress"
 - SHOW MASTER STATUS; # file 복사
-- GRANT REPLICATION SLAVE ON *.* TO '유저'@'%' IDENTIFIED BY '비번';
+- GRANT REPLICATION SLAVE ON *.* TO '유저'@'%';
 - FLUSH PRIVILEGES;
 
 # SLAVE DB
 - CHANGE MASTER TO MASTER_HOST='host 주소', MASTER_USER='root 이름', MASTER_PASSWORD='root 비번', MASTER_LOG_FILE='mysql-bin.xxxx', MASTER_LOG_POS=0, GET_MASTER_PUBLIC_KEY=1;
 - start slave;
 - show slave status\G;
+
+
+# 복붙
+mysql-bin :
+master-ip :
+command:
