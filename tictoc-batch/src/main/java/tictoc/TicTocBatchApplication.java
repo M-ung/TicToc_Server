430e94b411f40875615a124d1ac595f6629352d2
M-ung <wjdahrrla@gmail.com>
2025-03-04 00:18:19 +0900

    Feat: Batch 모듈에 ci/cd 파이프라인 구축해라 - #87

--- tictoc-batch/src/main/java/tictoc/TicTocBatchApplication.java
+++ tictoc-batch/src/main/java/tictoc/TicTocBatchApplication.java
@@ -1,9 +1,13 @@
 package tictoc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
 public class TicTocBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicTocBatchApplication.class, args);
+    }
 }
