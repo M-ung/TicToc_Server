2ca01f39c0e3aad04e6107a5b8e1e587c9dfd2bd
M-ung <wjdahrrla@gmail.com>
2025-03-03 23:58:51 +0900

    Feat: 로그인 기록 일주일 단위로 초기화 기능 스프링 배치 적용해서 구현해라 - #87

--- tictoc-batch/src/main/java/tictoc/TicTocBatchApplication.java
+++ tictoc-batch/src/main/java/tictoc/TicTocBatchApplication.java
@@ -1,7 +1,9 @@
 package tictoc;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TicTocBatchApplication {
 }
