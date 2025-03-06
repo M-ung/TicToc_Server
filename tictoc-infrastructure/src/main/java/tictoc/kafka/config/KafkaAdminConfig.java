27624819affa075c740aac20638d17baa5968b63
M-ung <wjdahrrla@gmail.com>
2025-03-06 15:55:29 +0900

    Fix: KafkaAdmin 반환 타입으로 인해 @Bean 에러 해결해라 - #100

--- tictoc-infrastructure/src/main/java/tictoc/kafka/config/KafkaAdminConfig.java
+++ tictoc-infrastructure/src/main/java/tictoc/kafka/config/KafkaAdminConfig.java
@@ -6,10 +6,13 @@ import org.apache.kafka.clients.admin.Config;
 import org.apache.kafka.clients.admin.ConfigEntry;
 import org.apache.kafka.common.config.ConfigResource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
 import tictoc.kafka.exception.KafkaSettingException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import static tictoc.error.ErrorCode.KAFKA_SETTING_ERROR;
 
@@ -20,17 +23,26 @@ public class KafkaAdminConfig {
     private String bootstrapServers;
 
     @Bean
-    public void configureTopicRetention() {
-        try (AdminClient adminClient = AdminClient.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers))) {
-            ConfigResource resource = new ConfigResource(ConfigResource.Type.TOPIC, "user-login-history-topic");
-            Map<ConfigResource, Config> updateConfigs = Map.of(
-                    resource, new Config(Collections.singletonList(
-                            new ConfigEntry("retention.ms", "60000"))
-                    )
-            );
-            adminClient.alterConfigs(updateConfigs).all().get();
-        } catch (Exception e) {
-            throw new KafkaSettingException(KAFKA_SETTING_ERROR);
-        }
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public ApplicationRunner configureTopicRetention(KafkaAdmin kafkaAdmin) {
+        return args -> {
+            try (AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
+                ConfigResource resource = new ConfigResource(ConfigResource.Type.TOPIC, "user-login-history-topic");
+                Map<ConfigResource, Config> updateConfigs = Map.of(
+                        resource, new Config(Collections.singletonList(
+                                new ConfigEntry("retention.ms", "60000"))
+                        )
+                );
+                adminClient.alterConfigs(updateConfigs).all().get();
+            } catch (Exception e) {
+                throw new KafkaSettingException(KAFKA_SETTING_ERROR);
+            }
+        };
     }
 }
