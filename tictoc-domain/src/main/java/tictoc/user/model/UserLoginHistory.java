41e3704fccda2a0aa3f7cd69952f1c5d9b9c8acf
M-ung <wjdahrrla@gmail.com>
2025-03-07 13:11:34 +0900

    Test: Spring Batch 배포할 때 테스트를 위해 스케줄러 수정해라 - #107

--- tictoc-domain/src/main/java/tictoc/user/model/UserLoginHistory.java
+++ tictoc-domain/src/main/java/tictoc/user/model/UserLoginHistory.java
@@ -2,10 +2,8 @@ package tictoc.user.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 import java.time.LocalDateTime;
 
-@Slf4j
 @Getter
 @Builder
 @AllArgsConstructor
