2ca9fd09180d16d4f38466be0246a60d78ae1779
M-ung <wjdahrrla@gmail.com>
2025-05-11 00:06:47 +0900

    Test: 인증 코드 잘못될 때 실패 테스트 작성해라 - #124

--- tictoc-api/src/test/java/tictoc/user/application/UserCommandServiceTest.java
+++ tictoc-api/src/test/java/tictoc/user/application/UserCommandServiceTest.java
@@ -7,24 +7,20 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import tictoc.config.security.jwt.RefreshToken;
 import tictoc.config.security.jwt.dto.JwtResDTO;
 import tictoc.config.security.jwt.util.JwtProvider;
 import tictoc.error.ErrorCode;
+import tictoc.error.exception.BadRequestException;
 import tictoc.kakao.KakaoFeignProvider;
-import tictoc.kakao.dto.KakaoResDTO;
 import tictoc.model.tictoc.TicTocStatus;
-import tictoc.profile.model.Profile;
-import tictoc.profile.model.ProfileImage;
 import tictoc.profile.port.ProfileRepositoryPort;
 import tictoc.user.model.User;
 import tictoc.user.model.type.UserRole;
 import tictoc.user.port.UserLoginHistoryRepositoryPort;
 import tictoc.user.port.UserRepositoryPort;
-import java.time.LocalDateTime;
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -74,5 +70,18 @@ class UserCommandServiceTest {
         assertThat(result.accessToken()).isEqualTo("dummy-access-token");
         assertThat(result.refreshToken()).isEqualTo("dummy-refresh-token");
         verify(userLoginHistoryRepositoryPort).save(eq(userId), any(), eq("1.1.1.1"), eq("chrome"));
+    }
+
+    @Test
+    @DisplayName("잘못된 인증 코드로 로그인 실패 테스트")
+    void 잘못된_인증_코드로_로그인_실패_테스트() {
+        // given
+        String authCode = "invalid-code";
+        when(kakaoFeignProvider.getKakaoAccessToken(authCode)).thenThrow(mock(FeignException.BadRequest.class));
+
+        // when & then
+        assertThatThrownBy(() -> userCommandService.login(authCode, "0.0.0.0", "Safari")
+        ).isInstanceOf(BadRequestException.class)
+                .hasMessageContaining(ErrorCode.KAKAO_BAD_REQUEST.getMessage());
     }
 }
